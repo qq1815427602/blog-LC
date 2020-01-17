c8219643d813def2ab9429e81352f59bd63cff32
zuiren <1045683477@qq.com>
2020-01-17 20:08:10 +0800

    first commit

---
+++ src/main/java/com/BlogLcApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogLcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogLcApplication.class, args);
+    }
+
+}
