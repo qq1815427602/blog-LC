241712646132a87864643af7b0cc4e6a1a08ad67
mocuishle <1045683477@qq.com>
2020-01-31 15:41:52 +0800

    更新security扫描不到的问题

--- blog-system/src/main/java/com/BlogLcApplication.java
+++ blog-system/src/main/java/com/BlogLcApplication.java
@@ -1,10 +1,15 @@
 package com;
 
+import com.security.config.IgnoreUrlsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
+/*@EnableConfigurationProperties({
+        IgnoreUrlsConfig.class
+})*/
 public class BlogLcApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogLcApplication.class, args);
