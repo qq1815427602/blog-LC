c7c89ebaa71ee772a73f6ed376bbfca2ce879117
夜空 <1815427602@qq.com>
2020-02-03 11:50:25 +0800

    service层添加 文章保存到编辑完成状态API 文章标签 删除 添加 修改 模糊分页查询API 分页工具类

--- blog-service/src/main/java/com/service/articles/ReLabelsService.java
+++ blog-service/src/main/java/com/service/articles/ReLabelsService.java
@@ -1,5 +1,10 @@
 package com.service.articles;
 
+import com.domain.articles.ReLabels;
+
+import java.util.List;
+import java.util.Map;
+
 /**
 * @Description:  标签业务逻辑层接口
 * @Author: 夜空
@@ -7,5 +12,33 @@ package com.service.articles;
 */
 
 public interface ReLabelsService {
+
+    /**
+     * @Description:  删除标签
+     * @Author: 夜空
+     * @Date: 2020/2/2
+     */
+    int deleteLabels(ReLabels reLabels);
+
+    /**
+     * @Description:  添加标签
+     * @Author: 夜空
+     * @Date: 2020/2/2
+     */
+    int addLabels(ReLabels reLabels);
+
+    /**
+     * @Description:  修改标签
+     * @Author: 夜空
+     * @Date: 2020/2/2
+     */
+    int updateLabels(ReLabels reLabels);
+
+    /**
+     * @Description:  分页模糊查询标签
+     * @Author: 夜空
+     * @Date: 2020/2/2
+     */
+    List<ReLabels> pagingFuzzyQueryLabels(Map<String, Object> map);
 
 }
--- blog-service/src/main/java/com/service/articles/impl/ReArticlesServiceImpl.java
+++ blog-service/src/main/java/com/service/articles/impl/ReArticlesServiceImpl.java
@@ -1,7 +1,14 @@
 package com.service.articles.impl;
 
+import com.common.paging.Pagetion;
+import com.dao.articles.ReArticlesDao;
+import com.domain.articles.ReArticles;
 import com.service.articles.ReArticlesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
 * @Description: 帖子业务逻辑层接口实现
@@ -12,4 +19,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReArticlesServiceImpl implements ReArticlesService {
 
+    @Autowired
+    ReArticlesDao reArticlesDao;
+
+    /**
+     * @Description: 保存文章为编辑完成状态
+     * @author: 夜空
+     * @date: 2020/2/1
+     */
+    @Override
+    public int ArticleSavedAsEditComplete(ReArticles reArticles) {
+        return reArticlesDao.ArticleSavedAsEditComplete(reArticles);
+    }
+
+    /**
+     * @Description:  模糊分页查询编辑完成文章
+     * @Author: 夜空
+     * @Date: 2020/2/1
+     */
+    @Override
+    public List<ReArticles> pagingFuzzyQueryEditorialFinishingArticles(Map<String, Object> map) {
+
+        //获取Map中的page键对应的值
+        Pagetion page =(Pagetion) map.get("pagetion");
+
+        //设置总记录数
+        page.setTotalCount(reArticlesDao.pagingFuzzyQueryEditorialFinishingArticlesAmount(map));
+
+        //存入当前页码
+        map.put("pageNo", (page.getPageNo() -1) * page.getPageSize());
+
+        //每页显示数量
+        map.put("pageSize", page.getPageSize());
+
+        return reArticlesDao.pagingFuzzyQueryEditorialFinishingArticles(map);
+
+    }
 }
